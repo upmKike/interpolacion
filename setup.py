850a53e3e858a7afeaf33cc555f1d40b73f64cc1
Kike <kikolo3000@gmail.com>
2022-11-10 15:47:53 +0100

    Add setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='interpolatcion',
+    version='0.1.0',    
+    description='A Python package to deal with interpolation, created by the students of "Algorítmica Numérica - Grado en Ciencia de datos e IA", from "Univesidad Politécnica de Madrid", course 2022/23',
+    url='https://github.com/upmKike/interpolacion',
+    license='MIT',
+    install_requires=['pandas', 'sympy', 'scipy', 'matplotlib', 'numpy', 'typing', 'shapely']
+)
