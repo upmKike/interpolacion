1cf912552190c26ea137189d27aca2b630f1dcc9
Kike <kikolo3000@gmail.com>
2022-11-10 15:59:05 +0100

    main and init tests

---
+++ main.py
@@ -0,0 +1,7 @@
+import src.int_hermite
+
+values = [(-1, 2, -8, 56), (0, 1, 0, 0), (1, 2, 8, 56)] 
+print('The degree of the polynomial will be ' + str((len(values)) * (len(values[0]) - 1) - 1))
+x, pyramid = src.int_hermite.divided_diff(values)
+print(x)
+print(pyramid)
