182f2d774381e1c99e6cf0c7c9d1662002af089f
Kike <kikolo3000@gmail.com>
2022-11-13 22:41:35 +0100

    typo

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='interpolatcion',
+    name='interpolacion',
     version='0.1.0',    
     description='A Python package to deal with interpolation, created by the students of "Algorítmica Numérica - Grado en Ciencia de datos e IA", from "Univesidad Politécnica de Madrid", course 2022/23',
     url='https://github.com/upmKike/interpolacion',
